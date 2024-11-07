b2335270803391b9c02bec342c2b47160c81125b
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2024-11-07 13:05:29 -0300

    Fix: flex-wrap in the input container

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -6,11 +6,11 @@ export default function ToDoInput(props) {
       <p className="text-3xl font-medium text-center pb-6">
         Get Things Done!
       </p>
-      <div className="flex gap-2">
+      <div className="flex flex-wrap gap-2">
         <input autoComplete="off" value={todoValue} onChange={(e) => {
           setTodoValue(e.target.value)
         }} className="p-2.5 bg-transparent flex-grow border border-solid border-[#845AFC] rounded-md focus:outline-none focus:border-[#bca7f5] transition-colors duration-300" type="text" name="todo" id="todo" placeholder="What is the task today?" />
-        <button disabled={!todoValue} className="disabled:opacity-50 disabled:hover transition-opacity bg-[#845AFC] enabled:hover:bg-[#6934fa] transition-colors duration-300 text-white px-3 rounded-md" onClick={() => {
+        <button disabled={!todoValue} className="flex-grow h-[2.875rem] disabled:opacity-50 disabled:hover transition-opacity bg-[#845AFC] enabled:hover:bg-[#6934fa] transition-colors duration-300 text-white px-3 rounded-md" onClick={() => {
           if (todoValue.trim() != '') {
             handleAddTodo(todoValue)
             setTodoValue('')
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,7 +4,7 @@ export default function ToDoList(props) {
   const {todos} = props
 
   return (
-    <ul className="flex flex-col gap-4 mt-4 max-h-[28.125rem] sm:max-h-[22.1875rem] overflow-y-auto">
+    <ul className="flex flex-col gap-4 mt-12 max-h-[24rem] sm:max-h-[22.1875rem] overflow-y-auto">
       {todos.map((todo, todoIndex) => {
         return (
           <ToDoCard {...props} todo={todo} key={todoIndex} todoIndex={todoIndex}>
